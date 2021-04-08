c03a5bcee953b5b671a50b86fe5f8589e0e0f2d8
Caique Granja Maia <81537856+CaiqueG@users.noreply.github.com>
2021-04-07 21:02:11 -0300

    Update script.js
    Adding coments

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 function adicionarValor() {
-  var valorEmCelsius = document.querySelector("#valor");
-  var valorEmCelsius2 = valorEmCelsius.value;
+  var valorEmCelsius = document.querySelector("#valor");     // Seleciona o campo em branco
+  var valorEmCelsius2 = valorEmCelsius.value;                // Seleciona o que foi digitado no campo
   var valorEmFahrenheit = (valorEmCelsius2 * 9) / 5 + 32;
-  valorEmFahrenheit = valorEmFahrenheit.toFixed(2);
+  valorEmFahrenheit = valorEmFahrenheit.toFixed(2);          // Fixa a variável em duas casas depois da vírgula
   var valorEmFahrenheit2 =
-    "O valor em Fahrenheit é: " + valorEmFahrenheit + "°F";
-  var valorFinal = document.getElementById("valorFinal");
-  valorFinal.innerHTML = valorEmFahrenheit2;
+    "O valor em Fahrenheit é: " + valorEmFahrenheit + "°F"; 
+  var valorFinal = document.getElementById("valorFinal");   // Seleciona a div em que o valor final vai aparecer na tela
+  valorFinal.innerHTML = valorEmFahrenheit2;                // exibe o valor na tela
 }
